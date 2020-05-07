9894835b49739485a7c8afb95b3f2d3930c00188
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-07 16:19:03 +1000

    Made main function use a function pointer array to run modules in the main loop

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "language_basics/circle.h"
+#include "types/limits.h"
+#include "types/enums.h"
+#include "types/using_void.h"
+
+typedef void (*emptyMethod)();
+
+void (*funcArr[])() = {generateCircle, getLimits, viewEnums, useVoid};
 
 int main() {
-    generateCircle();
+    for (int i = 0; i < (sizeof(funcArr) / sizeof(emptyMethod)); i++) {
+        funcArr[i]();
+    }
     return 0;
 }
